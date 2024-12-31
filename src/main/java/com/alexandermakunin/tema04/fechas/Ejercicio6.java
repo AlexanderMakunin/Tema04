8646e08ed4f269678c53dfa09ebfa5130901146c
Alex <59027586+AlexanderMakunin@users.noreply.github.com>
2024-12-31 23:59:34 +0100

    Toques a las 6 parte 2 (no va bien mes y dias)

--- src/main/java/com/alexandermakunin/tema04/fechas/Ejercicio6.java
+++ src/main/java/com/alexandermakunin/tema04/fechas/Ejercicio6.java
@@ -24,12 +24,15 @@ public class Ejercicio6 {
         LocalDateTime ahora = LocalDateTime.now();
         String DiaHoy = ahora.format(dateTimeFormatter);
         System.out.println("El dia de hoy: " + DiaHoy);
+        //Obtener valores del ahora
         int diaAhora = ahora.getDayOfMonth();
         int mesAhora = ahora.getMonthValue();
         int horaAhora = ahora.getHour();
         int minutosAhora = ahora.getMinute();
         int segundosAhora = ahora.getSecond();
         LocalDateTime anyoNuevo = LocalDateTime.parse("01/01/2025 00:00:00", dateTimeFormatter);
+        String felicidades = anyoNuevo.format(dateTimeFormatter2);
+        //Obtener valores del año nuevo
         int diaAnyoNuevo = anyoNuevo.getDayOfMonth();
         int mesAnyoNuevo = anyoNuevo.getMonthValue();
         int horaAnyoNuevo = anyoNuevo.getHour();
@@ -40,11 +43,12 @@ public class Ejercicio6 {
         int hora = horaAhora - horaAnyoNuevo;
         int minutos = minutosAhora - minutosAnyoNuevo;
         int segundos = segundosAhora - segundosAnyoNuevo;
+        //Obtener el tiempo que falta
         LocalDateTime Segundos = ChronoUnit.SECONDS.addTo(anyoNuevo, -segundos);
         LocalDateTime Minutos = ChronoUnit.MINUTES.addTo(Segundos, -minutos);
         LocalDateTime Horas = ChronoUnit.HOURS.addTo(Minutos, -hora);
-        LocalDateTime Dias = ChronoUnit.DAYS.addTo(Horas, -dia -1);
-        LocalDateTime Meses = ChronoUnit.MONTHS.addTo(Dias, -mes -1);
+        LocalDateTime Dias = ChronoUnit.DAYS.addTo(Horas, -dia);
+        LocalDateTime Resultado = ChronoUnit.MONTHS.addTo(Dias, -mes);
         final int a = 1;
         //LocalTime minusSeconds(long seconds);
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -52,9 +56,13 @@ public class Ejercicio6 {
             @Override
             public void run() {
                 i = contador(a,i);
-                LocalDateTime queda = ChronoUnit.SECONDS.addTo(Meses, -i);
-                String fechaFormateada = queda.format(dateTimeFormatter2);
-                System.out.println("Queda: " + fechaFormateada);
+                LocalDateTime queda = ChronoUnit.SECONDS.addTo(Resultado, -i);
+                String TiempoFaltante = queda.format(dateTimeFormatter2);
+                if (TiempoFaltante.equals(felicidades)){
+                    System.out.println("Feliz año nuevo!!!");
+                } else {
+                    System.out.println("Queda: " + TiempoFaltante);
+                }
             }
         }, 0, 1000);
     }
